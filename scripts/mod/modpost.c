494eef89f2a605e7dd7ba8ab1412bc8e658e18f6
Shih-Wei Li <shihwei@cs.columbia.edu>
2018-09-10 22:22:58 +0000

    fixup! arm64: stage2: Move cache.S into .hyp.txt section

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -897,15 +897,9 @@ static void check_section(const char *modname, struct elf_info *elf,
 #define TEXT_SECTIONS ".text", ".text.unlikely", ".sched.text", \
 		".kprobes.text", ".cpuidle.text"
 
-#ifndef CONFIG_STAGE2_KERNEL
 #define OTHER_TEXT_SECTIONS ".ref.text", ".head.text", ".spinlock.text", \
 		".fixup", ".entry.text", ".exception.text", ".text.*", \
 		".coldtext"
-#else
-#define OTHER_TEXT_SECTIONS ".ref.text", ".head.text", ".spinlock.text", \
-		".fixup", ".entry.text", ".exception.text", ".text.*", \
-		".coldtext", ".hyp.text"
-#endif
 
 #define INIT_SECTIONS      ".init.*"
 #define MEM_INIT_SECTIONS  ".meminit.*"
