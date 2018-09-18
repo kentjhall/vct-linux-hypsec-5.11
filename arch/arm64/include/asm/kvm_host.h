f70f59c765cb0c70f7687c85e010a36fa3c1fb21
Shih-Wei Li <shihwei@cs.columbia.edu>
2018-09-18 16:48:06 -0400

    fixup! stage2: kvm: arm64: Added helpers to reference shadow_vcpu entries

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -352,7 +352,6 @@ struct kvm_vcpu_arch {
 
 #ifdef CONFIG_STAGE2_KERNEL
 #define vcpu_shadow_gp_regs(v)	(&(v)->arch.shadow_vcpu_ctxt->gp_regs)
-#define vcpu_shadow_sys_regs(v)	(&(v)->arch.shadow_vm_ctxt->sys_regs[(r)])
 #endif
 
 u64 vcpu_read_sys_reg(struct kvm_vcpu *vcpu, int reg);
