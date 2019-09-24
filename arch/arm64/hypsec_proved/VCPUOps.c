472f7484a9829355bd8ef8313d78f18d6a940ed1
Shih-Wei Li <shihwei@cs.columbia.edu>
2019-09-24 18:01:18 -0400

    compile VCPUOps.c [removed undef injection & decryption support]

--- arch/arm64/hypsec_proved/VCPUOps.c
+++ arch/arm64/hypsec_proved/VCPUOps.c
@@ -6,10 +6,10 @@
 
 void save_shadow_kvm_regs(u32 ctxtid, u64 ec)
 {
-    set_shadow_ctxt(ctxtid, EC, ec);
+    set_shadow_ctxt(ctxtid, V_EC, ec);
     if (ec == ARM_EXCEPTION_TRAP)
     {
-        u64 hsr = get_shadow_ctxt(ctxtid, ESR_EL2);
+        u64 hsr = get_shadow_ctxt(ctxtid, V_ESR_EL2);
         u64 hsr_ec = (hsr / ESR_ELx_EC_SHIFT) % ESR_ELx_EC_MASK;
         if (hsr_ec == ESR_ELx_EC_WFx)
             prep_wfx(ctxtid);
@@ -22,7 +22,8 @@ void save_shadow_kvm_regs(u32 ctxtid, u64 ec)
         else if (hsr_ec == ESR_ELx_EC_DABT_LOW)
             prep_abort(ctxtid);
         else
-            hypsec_inject_undef(ctxtid);
+            v_panic();
+            //hypsec_inject_undef(ctxtid);
     }
 }
 
@@ -33,11 +34,11 @@ void restore_shadow_kvm_regs(u32 ctxtid)
 
     if (dirty == INVALID64)
     {
-        if (vm_is_inc_exe(vmid) == 1U)
+        /*if (vm_is_inc_exe(vmid) == 1U)
         {
             int_to_shadow_decrypt(ctxtid);
         }
-        else
+        else*/
         {
             reset_gp_regs(ctxtid);
             reset_sys_regs(ctxtid);
@@ -47,31 +48,32 @@ void restore_shadow_kvm_regs(u32 ctxtid)
     }
     else
     {
-        u64 ec = get_shadow_ctxt(ctxtid, EC);
+        u64 ec = get_shadow_ctxt(ctxtid, V_EC);
         if (ec == ARM_EXCEPTION_TRAP)
             sync_dirty_to_shadow(ctxtid);
-        if (dirty & PENDING_EXCEPT_INJECT_FLAG)
-            update_exception_gp_regs(ctxtid);
+        //if (dirty & PENDING_EXCEPT_INJECT_FLAG)
+            //update_exception_gp_regs(ctxtid);
         if (dirty & DIRTY_PC_FLAG) {
-            u64 pc = get_shadow_ctxt(ctxtid, PC);
-            set_shadow_ctxt(ctxtid, PC, pc + 4UL);
+            u64 pc = get_shadow_ctxt(ctxtid, V_PC);
+            set_shadow_ctxt(ctxtid, V_PC, pc + 4UL);
         }
         set_shadow_ctxt(ctxtid, DIRTY, 0UL);
-        set_shadow_ctxt(ctxtid, FAR_EL2, 0UL);
+        set_shadow_ctxt(ctxtid, V_FAR_EL2, 0UL);
 
-        if (get_shadow_ctxt(ctxtid, FLAGS) & PENDING_FSC_FAULT)
+        if (get_shadow_ctxt(ctxtid, V_FLAGS) & PENDING_FSC_FAULT)
         {
-            post_handle_shadow_s2pt_fault(ctxtid);
+            //post_handle_shadow_s2pt_fault(ctxtid);
         }
 
-        set_shadow_ctxt(ctxtid, FLAGS, 0UL);
+        set_shadow_ctxt(ctxtid, V_FLAGS, 0UL);
     }
 }
 
+/*
 void save_encrypted_vcpu(u32 ctxtid)
 {
     shadow_to_int_encrypt(ctxtid);
     u64 pstate = get_shadow_ctxt(ctxtid, PSTATE);
     set_int_ctxt(ctxtid, PSTATE, pstate);
 }
-
+*/
