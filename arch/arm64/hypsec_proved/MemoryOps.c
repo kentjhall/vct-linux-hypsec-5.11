ad521405c57a35c810b6742f8ef07d1e50c8607d
Shih-Wei Li <shihwei@cs.columbia.edu>
2019-11-01 20:59:49 -0400

    sync code from xupeng's implementation

--- arch/arm64/hypsec_proved/MemoryOps.c
+++ arch/arm64/hypsec_proved/MemoryOps.c
@@ -8,36 +8,33 @@ void __hyp_text __clear_vm_stage2_range(u32 vmid, u64 start, u64 size)
 {
     u32 poweron = get_vm_poweron(vmid);
     if (size == KVM_PHYS_SIZE && poweron == 0U) {
-        u32 cnt = get_mem_region_cnt();
-        u32 i = 0U;
-        while (i < cnt)
-        {
-            u64 flag = get_mem_region_flag(i);
-            if ((flag & MEMBLOCK_NOMAP) == 0UL)
-            {
-                u64 b = get_mem_region_base(i);
-                u64 s = get_mem_region_size(i);
-                u64 pfn = b / PAGE_SIZE;
-                u64 num = s / PAGE_SIZE;
-                while (num > 0UL)
-                {
-                    clear_phys_mem(pfn);
-                    clear_vm_page(vmid, pfn);
-                    pfn += 1UL;
-                    num -= 1UL;
-                }
-            }
-            i += 1U;
-        }
+		u64 num = size / PAGE_SIZE;
+		u64 pfn = 0UL;
+		while (num > 0UL)  {
+			clear_vm_page(vmid, pfn);
+			pfn += 1UL;
+			num -= 1UL;
+		}
     }
 }
 
+#define PMD_PAGE_NUM	256
 void __hyp_text prot_and_map_vm_s2pt(u32 vmid, u64 fault_addr, u64 new_pte, u32 level, u32 iabt)
 {
-    u64 target_addr = phys_page(new_pte);
-    u64 target_pfn = target_addr / PAGE_SIZE;
-    //assign_pfn_to_vm(vmid, target_pfn);
-    map_pfn_vm(vmid, fault_addr, new_pte, level, iabt);
+	u64 target_addr = phys_page(new_pte);
+	u64 target_pfn = target_addr / PAGE_SIZE;
+	if (level == 2) {
+		u64 n = PMD_PAGE_NUM;
+		u64 pfn = target_pfn;
+		while (n > 0) {
+			assign_pfn_to_vm(vmid, pfn);
+			pfn += 1;
+			n -= 1;
+		}
+	} else {
+		assign_pfn_to_vm(vmid, target_pfn);
+	}
+	map_pfn_vm(vmid, fault_addr, new_pte, level, iabt);
 }
 
 void __hyp_text v_grant_stage2_sg_gpa(u32 vmid, u64 addr, u64 size)
