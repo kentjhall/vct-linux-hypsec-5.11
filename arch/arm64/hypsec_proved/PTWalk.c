0c3583392e5cca4a019e83389350708fd09333b7
Shih-Wei Li <shihwei@cs.columbia.edu>
2020-05-06 16:54:23 -0400

    updated PTWalk.c and added __dma_map_area

--- arch/arm64/hypsec_proved/PTWalk.c
+++ arch/arm64/hypsec_proved/PTWalk.c
@@ -103,6 +103,7 @@ u64 __hyp_text walk_smmu_pgd(u64 ttbr, u64 addr, u32 alloc)
             pgd_pa = alloc_smmu_pgd_page();
             pgd = pgd_pa | ARM_LPAE_PTE_TYPE_TABLE;
             smmu_pt_store(ttbr_pa + pgd_idx * 8UL, pgd);
+	    __dma_map_area(__el2_va(ttbr_pa + pgd_idx * 8UL), sizeof(u64), 1);
         }
         ret = pgd;
     }
@@ -115,13 +116,14 @@ u64 __hyp_text walk_smmu_pmd(u64 pgd, u64 addr, u32 alloc)
     u64 ret = 0UL;
     if (pgd_pa != 0UL) {
         u64 pmd_idx = pmd_index(addr);
-        u64 pmd = smmu_pt_load(pgd_pa | (pmd_idx * 8));
+        u64 pmd = smmu_pt_load(pgd_pa + pmd_idx * 8);
         u64 pmd_pa = phys_page(pmd);
         if (pmd_pa == 0UL && alloc == 1U)
         {
             pmd_pa = alloc_smmu_pmd_page();
             pmd = pmd_pa | ARM_LPAE_PTE_TYPE_TABLE;
-            smmu_pt_store(pgd_pa | (pmd_idx * 8UL), pmd);
+            smmu_pt_store(pgd_pa + pmd_idx * 8UL, pmd);
+	    __dma_map_area(__el2_va(pgd_pa + pmd_idx * 8UL), sizeof(u64), 1);
         }
         ret = pmd;
     }
@@ -134,7 +136,7 @@ u64 __hyp_text walk_smmu_pte(u64 pmd, u64 addr)
     u64 ret = 0UL;
     if (pmd_pa != 0UL) {
         u64 pte_idx = pte_index(addr);
-        ret = smmu_pt_load(pmd_pa | (pte_idx * 8UL));
+        ret = smmu_pt_load(pmd_pa + pte_idx * 8UL);
     }
     return ret;
 }
@@ -143,5 +145,6 @@ void __hyp_text set_smmu_pte(u64 pmd, u64 addr, u64 pte)
 {
     u64 pmd_pa = phys_page(pmd);
     u64 pte_idx = pte_index(addr);
-    smmu_pt_store(pmd_pa | (pte_idx * 8UL), pte);
+    smmu_pt_store(pmd_pa + pte_idx * 8UL, pte);
+    __dma_map_area(__el2_va(pmd_pa + pte_idx * 8UL), sizeof(u64), 1);
 }
