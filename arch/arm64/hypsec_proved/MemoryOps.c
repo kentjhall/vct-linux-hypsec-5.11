da82481755aacd35db1c50ecdcf632e11b93c090
Shih-Wei Li <shihwei@cs.columbia.edu>
2021-01-13 17:39:23 -0500

    WIP: MemoryOps.c

--- arch/arm64/hypsec_proved/MemoryOps.c
+++ arch/arm64/hypsec_proved/MemoryOps.c
@@ -4,44 +4,26 @@
  * MemoryOps
  */
 
-// Xupeng has this in the spec: void clear_vm_range(u32 vmid, u64 pfn, u64 num)
-void __hyp_text __clear_vm_range(u32 vmid, u64 start, u64 size)
+void __hyp_text clear_vm_range(u32 vmid, u64 pfn, u64 num)
 {
-	u64 pfn = start >> PAGE_SHIFT;
-	u64 num = size / PAGE_SIZE;
-	while (num > 0UL)  {
+	while (num > 0UL)
+	{
 		clear_vm_page(vmid, pfn);
 		pfn += 1UL;
 		num -= 1UL;
 	}
 }
 
-/*
-void __hyp_text __clear_vm_stage2_range(u32 vmid, u64 start, u64 size)
-{
-	u32 poweron = get_vm_poweron(vmid);
-	if (size == KVM_PHYS_SIZE && poweron == 0U) {
-		u32 i = 0U;
-		u64 size = get_phys_mem_size();
-		u64 num = size / PAGE_SIZE;
-		u64 pfn = get_phys_mem_start_pfn();
-		while (i < num) {
-			clear_vm_page(vmid, pfn);
-			pfn += 1;
-			i += 1;
-		}
-	}
-}
-*/
-
 #define PMD_PAGE_NUM	512
 void __hyp_text prot_and_map_vm_s2pt(u32 vmid, u64 addr, u64 pte, u32 level)
 {
-	u64 pfn, gfn, num;
-	u64 target_addr = phys_page(pte);
+	u64 pfn, gfn, num, target_addr;
+
+	target_addr = phys_page(pte);
 	pfn = target_addr / PAGE_SIZE;
 	gfn = addr / PAGE_SIZE;
 
+	//TODO: Xupeng does not have this, shall we?
 	if (pte == 0)
 		return;
 
@@ -49,32 +31,16 @@ void __hyp_text prot_and_map_vm_s2pt(u32 vmid, u64 addr, u64 pte, u32 level)
 		/* gfn is aligned to 2MB size */
 		gfn = gfn / PTRS_PER_PMD * PTRS_PER_PMD;
 		num = PMD_PAGE_NUM;
-		//ret = assign_pfn_to_vm(vmid, gfn, pfn, PMD_PAGE_NUM);
-		//if (ret == 1) {
-		//	print_string("\rsplitting pmd to pte\n");
-		//	new_pte += (agfn - gfn) * PAGE_SIZE;
-		//	map_pfn_vm(vmid, fault_addr, new_pte, 3U);
-		//}
-		//else if (ret == 0) {
-		//if (ret == 0) {
-			//map_pfn_vm(vmid, fault_addr, new_pte, 2U);
-		//}
+		while (num > 0UL) {
+			assign_pfn_to_vm(vmid, gfn, pfn);
+			gfn += 1UL;
+			pfn += 1UL;
+			num -= 1UL;
+		}
 	}
 	else {
-		/* agfn is aligned to 4KB size */
-		//ret = assign_pfn_to_vm(vmid, agfn, pfn, 1UL);
-		//if (ret == 0) {
-		//	map_pfn_vm(vmid, fault_addr, new_pte, 3U);
-		//}
-		num = 1UL;
-		level = 3U;
-	}
-
-	while (num > 0UL) {
 		assign_pfn_to_vm(vmid, gfn, pfn);
-		gfn += 1UL;
-		pfn += 1UL;
-		num -= 1UL;
+		level = 3U;
 	}
 
 	map_pfn_vm(vmid, addr, pte, level);
@@ -139,7 +105,19 @@ void __hyp_text v_revoke_stage2_sg_gpa(u32 vmid, u64 addr, u64 size)
     }
 }
 
-// FIXME: Xupeng does not have this
+//TODO: FIXME: Xupeng does not have this
+void __hyp_text __clear_vm_range(u32 vmid, u64 start, u64 size)
+{
+	u64 pfn = start >> PAGE_SHIFT;
+	u64 num = size / PAGE_SIZE;
+	while (num > 0UL)  {
+		clear_vm_page(vmid, pfn);
+		pfn += 1UL;
+		num -= 1UL;
+	}
+}
+
+//TODO: FIXME: Xupeng does not have this
 void __hyp_text __clear_vm_stage2_range(u32 vmid, u64 start, u64 size)
 {
 	u32 poweron = get_vm_poweron(vmid);
@@ -153,16 +131,5 @@ void __hyp_text __clear_vm_stage2_range(u32 vmid, u64 start, u64 size)
 				__clear_vm_range(vmid, base, sz);
 			i++;
 		}
-	}
-}
-
-// FIXME: Xupeng does not have this
-void __hyp_text clear_vm_range(u32 vmid, u64 pfn, u64 num)
-{
-	while (num > 0UL)
-	{
-		clear_vm_page(vmid, pfn);
-		pfn += 1UL;
-		num -= 1UL;
 	}
 }
