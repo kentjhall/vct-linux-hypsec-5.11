0a0f2eb952d2467ac851b360a22370290d9fa340
Shih-Wei Li <shihwei@cs.columbia.edu>
2021-01-13 12:47:31 -0500

    BootCore.c

--- arch/arm64/hypsec_proved/BootCore.c
+++ arch/arm64/hypsec_proved/BootCore.c
@@ -6,17 +6,19 @@
 
 u32 __hyp_text gen_vmid()
 {
-	u32 vmid;
+	u32 vmid = INVALID;
 	acquire_lock_core();
 	vmid = get_next_vmid();
 	if (vmid < MAX_VM_NUM) {
 		set_next_vmid(vmid + 1U);
 	}
-	else {
-		vmid = INVALID;
+	else
+	{
+		print_string("\rpanic in gen_vmid\n");
+		v_panic();
 	}
 	release_lock_core();
-	return vmid;
+	return check(vmid);
 }
 
 u64 __hyp_text alloc_remap_addr(u64 pgnum)
@@ -27,9 +29,9 @@ u64 __hyp_text alloc_remap_addr(u64 pgnum)
 	if (remap + pgnum * PAGE_SIZE < EL2_REMAP_END)
 		set_next_remap_ptr(remap + pgnum * PAGE_SIZE);
 	else {
-		print_string("\rpanic in remap\n");
+		print_string("\rpanic in alloc_remap_addr\n");
 		v_panic();
 	}
 	release_lock_core();
-	return remap;
+	return check64(remap);
 }
