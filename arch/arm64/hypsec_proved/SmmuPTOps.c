c351808e89b78f2797ec6456742c30996dce2db3
Shih-Wei Li <shihwei@cs.columbia.edu>
2021-01-14 17:11:10 -0500

    SmmuPTOps.c

--- arch/arm64/hypsec_proved/SmmuPTOps.c
+++ arch/arm64/hypsec_proved/SmmuPTOps.c
@@ -6,34 +6,39 @@
 
 void __hyp_text init_spt(u32 cbndx, u32 index)
 {
-    acquire_lock_spt();
-    clear_smmu_pt(cbndx, index);
-    release_lock_spt();
+	acquire_lock_spt();
+	clear_smmu_pt(cbndx, index);
+	release_lock_spt();
 }
 
 u64 __hyp_text walk_spt(u32 cbndx, u32 index, u64 addr)
 {
-    u64 ret;
+	u64 ret;
 
-    acquire_lock_spt();
-    ret = walk_smmu_pt(cbndx, index, addr);
-    release_lock_spt();
-    return ret;
+	acquire_lock_spt();
+	ret = walk_smmu_pt(cbndx, index, addr);
+	release_lock_spt();
+	return ret;
 }
 
 void __hyp_text map_spt(u32 cbndx, u32 index, u64 addr, u64 pte)
 {
-    acquire_lock_spt();
-    set_smmu_pt(cbndx, index, addr, pte);
-    release_lock_spt();
+	acquire_lock_spt();
+	set_smmu_pt(cbndx, index, addr, pte);
+	release_lock_spt();
 }
 
 u64 __hyp_text unmap_spt(u32 cbndx, u32 index, u64 addr) 
 {
-    u64 ret;
+	u64 ret;
 
-    acquire_lock_spt();
-    ret = unmap_smmu_pt(cbndx, index, addr);
-    release_lock_spt();
-    return ret;
+	acquire_lock_spt();
+	ret = walk_smmu_pt(cbndx, index, addr);
+	if (ret != 0UL)
+	{
+		set_smmu_pt(cbndx, index, addr, 0UL);
+		//ret = unmap_smmu_pt(cbndx, index, addr);
+	}
+	release_lock_spt();
+	return check64(ret);
 }
