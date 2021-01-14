90cc920b9c82e06f44cfe445d1aa294848c78119
Shih-Wei Li <shihwei@cs.columbia.edu>
2021-01-13 19:40:07 -0500

    update ptalloc.c

--- arch/arm64/hypsec_proved/PTAlloc.c
+++ arch/arm64/hypsec_proved/PTAlloc.c
@@ -6,8 +6,10 @@
 
 u64 __hyp_text alloc_s2pt_pgd(u32 vmid)
 {
-	u64 next = get_pgd_next(vmid);
-	u64 end = pgd_pool_end(vmid);
+	u64 next, end;
+
+	next = get_pgd_next(vmid);
+	end = pgd_pool_end(vmid);
 
 	if (next + PAGE_SIZE <= end) {
 		set_pgd_next(vmid, 1);
@@ -18,13 +20,15 @@ u64 __hyp_text alloc_s2pt_pgd(u32 vmid)
 		v_panic();
 	}
 
-	return next;
+	return check64(next);
 }
 
 u64 __hyp_text alloc_s2pt_pud(u32 vmid)
 {
-	u64 next = get_pud_next(vmid);
-	u64 end = pud_pool_end(vmid);
+	u64 next, end;
+
+	next = get_pud_next(vmid);
+	end = pud_pool_end(vmid);
 
 	if (next + PAGE_SIZE <= end) {
 		set_pud_next(vmid, 1);
@@ -35,14 +39,15 @@ u64 __hyp_text alloc_s2pt_pud(u32 vmid)
 		v_panic();
 	}
 
-	return next;
+	return check64(next);
 }
 
 u64 __hyp_text alloc_s2pt_pmd(u32 vmid)
 {
-	u64 next = get_pmd_next(vmid);
-	u64 end = pmd_pool_end(vmid);
-	u64 ret = next;
+	u64 next, end;
+
+	next = get_pmd_next(vmid);
+	end = pmd_pool_end(vmid);
 
 	if (next + PAGE_SIZE <= end) {
 		set_pmd_next(vmid, 1);
@@ -55,39 +60,3 @@ u64 __hyp_text alloc_s2pt_pmd(u32 vmid)
 
 	return next;
 }
-
-/*
-u64 __hyp_text alloc_smmu_pgd_page(void)
-{
-	u64 next = get_smmu_pgd_next();
-	u64 end = smmu_pgd_end();
-
-	//print_string("\ralloc smmu pgd page\n");
-	//printhex_ul(next);
-	if (next + PAGE_SIZE <= end) {
-		set_smmu_pgd_next(1);
-	}
-	else {
-	        print_string("\rwe used all smmu pgd pages\n");
-		v_panic();
-	}
-	return next;
-}
-
-u64 __hyp_text alloc_smmu_pmd_page(void)
-{
-	u64 next = get_smmu_pmd_next();
-	u64 end = smmu_pmd_end();
-
-	//print_string("\ralloc smmu pmd page\n");
-	//printhex_ul(next);
-	if (next + PAGE_SIZE <= end) {
-		set_smmu_pmd_next(1);
-	}
-	else {
-	        print_string("\rwe used all smmu pmd pages\n");
-		v_panic();
-	}
-	return next;
-}
-*/
