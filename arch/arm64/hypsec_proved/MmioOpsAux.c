5ce93be86de78f0ebf9fd8fd21a9df4bd1d00ffd
Shih-Wei Li <shihwei@cs.columbia.edu>
2021-01-14 15:39:30 -0500

    MmioOpsAux

--- arch/arm64/hypsec_proved/MmioOpsAux.c
+++ arch/arm64/hypsec_proved/MmioOpsAux.c
@@ -1,16 +1,22 @@
 #include "hypsec.h"
 #include "MmioOps.h"
 
+//TODO: change it to return u32
 u64 __hyp_text is_smmu_range(u64 addr)
 {
-	u32 total_smmu = get_smmu_num();
-	u32 i = 0U;
-	u64 res = INVALID64;
+	u32 total_smmu, i;
+	u64 res, base, size;
 
-	while (i < total_smmu) {
-		u64 base = get_smmu_base(i);
-		u64 size = get_smmu_size(i);
-		if ((base <= addr) && (addr < base + size)) {
+	total_smmu = get_smmu_num();
+	i = 0U;
+	res = INVALID64;
+
+	while (i < total_smmu)
+	{
+		base = get_smmu_base(i);
+		size = get_smmu_size(i);
+		if ((base <= addr) && (addr < base + size))
+		{
 			res = i;
 		}
 		i = i + 1U;
@@ -23,8 +29,7 @@ void __hyp_text handle_host_mmio(u64 index, u32 hsr)
 {
 	u64 base_addr;
 	u64 fault_ipa;
-	u32 is_write;
-	u32 len;
+	u32 is_write, len;
 
 	/* Following three lines are maco */
 	base_addr = get_smmu_hyp_base(index);
@@ -32,16 +37,14 @@ void __hyp_text handle_host_mmio(u64 index, u32 hsr)
 	len = host_dabt_get_as(hsr);
 	is_write = host_dabt_is_write(hsr);
 
-	if (is_write) {
-		//print_string("\rhandle_host_mmuio write\n");
-		//printhex_ul(fault_ipa);
-		handle_smmu_write(hsr, fault_ipa, len, index);
-		//print_string("\rafter handle_host_mmuio write\n");
-	} else {
-		//print_string("\rhandle_host_mmuio read\n");
-		//printhex_ul(fault_ipa);
+	if (is_write == 0U)
+	{
 		handle_smmu_read(hsr, fault_ipa, len);
-		//print_string("\rafter handle_host_mmuio read\n");
+		host_skip_instr();
 	}
-	host_skip_instr();
+	else
+	{
+		handle_smmu_write(hsr, fault_ipa, len, index);
+		host_skip_instr();
+	}
 }
