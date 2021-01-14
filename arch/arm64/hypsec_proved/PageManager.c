627e78017f580680481bc9e218e4cab52662df3e
Shih-Wei Li <shihwei@cs.columbia.edu>
2021-01-13 20:59:15 -0500

    PageManager.c

--- arch/arm64/hypsec_proved/PageManager.c
+++ arch/arm64/hypsec_proved/PageManager.c
@@ -4,50 +4,73 @@
  * PageManager
  */
 
-//Synchronized
 u32 __hyp_text get_pfn_owner(u64 pfn)
 {
-	u64 index = get_s2_page_index(pfn * PAGE_SIZE);
-	u32 ret = INVALID_MEM;
-	if (index != INVALID64) {
+	u64 index;
+	u32 ret;
+
+	index = get_s2_page_index(pfn * PAGE_SIZE);
+	if (index != INVALID64)
+	{
 		ret = get_s2_page_vmid(index);
 	}
-	return ret;
+	else
+	{
+		ret = INVALID_MEM;
+	}
+	return check(ret);
 }
 
 void __hyp_text set_pfn_owner(u64 pfn, u32 vmid)
 {
-	u64 index = get_s2_page_index(pfn * PAGE_SIZE);
+	u64 index;
+
+	index = get_s2_page_index(pfn * PAGE_SIZE);
 	if (index != INVALID64)
+	{
 		set_s2_page_vmid(index, vmid);
+	}
 }
 
 u32 __hyp_text get_pfn_count(u64 pfn)
 {
-	u64 index = get_s2_page_index(pfn * PAGE_SIZE);
-	u32 ret = INVALID_MEM;
-	if (index != INVALID64) {
+	u64 index;
+	u32 ret;
+
+	index = get_s2_page_index(pfn * PAGE_SIZE);
+	if (index != INVALID64)
+	{
 		ret = get_s2_page_count(index);
+	} else
+	{
+		ret = INVALID_MEM;
 	}
 	return ret;
 }
 
 void __hyp_text set_pfn_count(u64 pfn, u32 count)
 {
-	u64 index = get_s2_page_index(pfn * PAGE_SIZE);
-	if (index != INVALID64) {
+	u64 index;
+
+	index = get_s2_page_index(pfn * PAGE_SIZE);
+	if (index != INVALID64)
+	{
 		set_s2_page_count(index, count);
 	}
 }
 
 u64 __hyp_text get_pfn_map(u64 pfn)
 {
-	u64 index = get_s2_page_index(pfn * PAGE_SIZE);
+	u64 index;
 	u64 ret;
-	if (index != INVALID64) {
+
+	index = get_s2_page_index(pfn * PAGE_SIZE);
+	if (index != INVALID64)
+	{
 		ret = get_s2_page_gfn(index);
 	}
-	else {
+	else
+	{
 		ret = INVALID_MEM;
 	}
 	return ret;
@@ -55,8 +78,11 @@ u64 __hyp_text get_pfn_map(u64 pfn)
 
 void __hyp_text set_pfn_map(u64 pfn, u64 gfn)
 {
-	u64 index = get_s2_page_index(pfn * PAGE_SIZE);
-	if (index != INVALID64) {
+	u64 index;
+
+	index = get_s2_page_index(pfn * PAGE_SIZE);
+	if (index != INVALID64)
+	{
 		set_s2_page_gfn(index, gfn);
 	}
 }
